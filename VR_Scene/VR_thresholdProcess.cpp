dab7ce0d50b38abcd9ed66d4571c2c1125cb48e4
marco <marc.olivier.girard.3@gmail.com>
2018-03-21 20:20:21 -0400

    Première transformation pixel par pixel dans thresholdProcess
    tout baigne

--- VR_Scene/VR_thresholdProcess.cpp
+++ VR_Scene/VR_thresholdProcess.cpp
@@ -15,8 +15,52 @@ VR_thresholdProcess::~VR_thresholdProcess()
 
 void VR_thresholdProcess::process(QImage & imgIn, QImage & imgOut)
 {
-	int *outCur = reinterpret_cast<int*>((uchar*)(imgIn.bits()));
 	imgOut = imgIn;
+	int *inCur = reinterpret_cast<int*>((uchar*)(imgIn.bits()));
+	int *outCur = reinterpret_cast<int*>((uchar*)(imgOut.bits()));
+	int *outEnd = outCur + imgIn.width() * imgIn.height();
+	int currentPixel;
+	uchar r, g, b;
+	
+	while (outCur < outEnd) {
+		currentPixel = *inCur;
+
+		currentPixel = *outCur;
+		
+		*outCur = (currentPixel ^= 0x00FF0000);
+		
+		++outCur;
+		++inCur;
+	}
+
+
+
+	//int *outCur = reinterpret_cast<int*>((uchar*)(im.bits()));
+	//int *outEnd = outCur + im.width() * im.height();
+	//int value;
+	//uchar r, g, b;
+	//uchar rMin = param1_->lowValue();
+	//uchar rMax = param1_->highValue();
+	//uchar gMin = param2_->lowValue();
+	//uchar gMax = param2_->highValue();
+	//uchar bMin = param3_->lowValue();
+	//uchar bMax = param3_->highValue();
+	//bool keepSource = checkedButtonKeepImage_->isChecked();
+
+	//while (outCur < outEnd) {
+	//	value = *outCur;
+	//	r = (value & 0x00FF0000) >> 16;
+	//	g = (value & 0x0000FF00) >> 8;
+	//	b = value & 0x000000FF;
+
+	//	if (r >= rMin && r <= rMax && g >= gMin && g <= gMax && b >= bMin && b <= bMax) {
+	//		*outCur = keepSource ? value : 0xFFFFFFFF;
+	//	}
+	//	else {
+	//		*outCur = 0xFF000000;
+	//	}
+	//	++outCur;
+	//}
 
 }
 
