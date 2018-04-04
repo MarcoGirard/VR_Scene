86372e21cf5fbecccfd5ae5b5d1ea687e17d237f
marco <marc.olivier.girard.3@gmail.com>
2018-04-04 10:15:32 -0400

    BlurProcess maintenant plus stable
    Il reste à débugger pourquoi il assombrie

--- VR_Scene/VR_BlurProcess.cpp
+++ VR_Scene/VR_BlurProcess.cpp
@@ -14,56 +14,63 @@ VR_BlurProcess::~VR_BlurProcess()
 
 
 //https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/hough_circle/hough_circle.html
+
 void VR_BlurProcess::process(const Mat &imgIn, Mat &imgOut)
 {
 	imgIn.copyTo(imgOut);
 
-	int const * row{ nullptr };
-	int const *currentinpixel{ nullptr };
-	int pixelout;
-	double totalr{ 0.0 }, totalg{ 0.0 }, totalb{ 0.0 };
-	int imgwidth{ imgIn.cols }, imgheight{ imgIn.rows };
+	double totalR{ 0.0 }, totalG{ 0.0 }, totalB{ 0.0 };
+	int imgWidth{ imgIn.cols }, imgHeight{ imgIn.rows };
 	uchar r, g, b;
 	int currentKernelSize = kernelSize;
 
-	const int horizontaloffset{ static_cast<int>((currentKernelSize - 1) / 2) };
-	const int verticaloffset{ static_cast<int>(imgwidth * horizontaloffset) };
+	int iterationWidth{ imgWidth - (currentKernelSize - 1)};
+	int iterationHeight{ imgHeight - (currentKernelSize - 1) };
+	int horizontalOffset{ currentKernelSize / 2 };
+	int verticalOffset{ imgWidth * horizontalOffset };
+	int rowGap{ horizontalOffset * 2};
 	
-	int * curout{ reinterpret_cast<int*>(imgOut.ptr()) };
-	curout += verticaloffset + horizontaloffset; // placer le pointeur du pixel de sortie 
+	int const *currentInPixel{ reinterpret_cast<int const*>(imgIn.ptr()) };
+	int * curOut{ reinterpret_cast<int*>(imgOut.ptr()) };
+	int const * kernelPixel{ nullptr };
+
+	unsigned int initialPointerOffset = (currentKernelSize / 2) + ((currentKernelSize / 2)*imgWidth);
+	currentInPixel += initialPointerOffset;
+	curOut += initialPointerOffset;
 
 	/* loop through imgs */
-	for (int i{ horizontaloffset }; i < (imgheight - horizontaloffset); ++i) {
-		row = reinterpret_cast<int const *>(imgIn.ptr(i));
-		curout += 2 * horizontaloffset; // on ignore les x premières et dernières colonnes au changement de ligne
-		for (int j{ horizontaloffset }; j < (imgwidth - horizontaloffset); ++j) {
-			currentinpixel = &row[j];
-			*curout = 0;
-			totalr = totalg = totalb = 0;
+	for (int i{ 0 }; i < iterationHeight; ++i) {
+		for (int j{ 0 }; j < iterationWidth; ++j) {
+			kernelPixel = currentInPixel;
+			*curOut = 0;
+			totalR = totalG = totalB = 0;
 			
 			/* loop around pixel */
-			for (int l{ -verticaloffset }; l <= verticaloffset; l += verticaloffset){
-				currentinpixel += l;
-				for (int k{ -horizontaloffset }; k <= horizontaloffset; ++k) {
-					currentinpixel += k;
+			for (int l{ -verticalOffset }; l <= verticalOffset; l += verticalOffset){
+				kernelPixel += l;
+				for (int k{ -horizontalOffset }; k <= horizontalOffset; ++k) {
+					kernelPixel += k;
 
-					totalr += (*currentinpixel & 0x00ff0000) >> 16;
-					totalg += (*currentinpixel & 0x0000ff00) >> 8;
-					totalb += (*currentinpixel & 0x000000ff);
+					totalR += (*kernelPixel & 0x00ff0000) >> 16;
+					totalG += (*kernelPixel & 0x0000ff00) >> 8;
+					totalB += (*kernelPixel & 0x000000ff);
 				}
 			}
-			totalr /= (currentKernelSize * currentKernelSize);
-			totalg /= (currentKernelSize * currentKernelSize);
-			totalb /= (currentKernelSize * currentKernelSize);
+			totalR /= (currentKernelSize * currentKernelSize);
+			totalG /= (currentKernelSize * currentKernelSize);
+			totalB /= (currentKernelSize * currentKernelSize);
 
-			*curout = 0xFF000000;
-			*curout |= (static_cast<unsigned int>(std::floor(totalr)) << 16);
-			*curout |= (static_cast<unsigned int>(std::floor(totalg)) << 8);
-			*curout |= (static_cast<unsigned int>(std::floor(totalb)));
+			*curOut = 0xFF000000;
+			*curOut |= (static_cast<unsigned int>(std::floor(totalR)) << 16);
+			*curOut |= (static_cast<unsigned int>(std::floor(totalG)) << 8);
+			*curOut |= (static_cast<unsigned int>(std::floor(totalB)));
 
 			/* pendant l'itération sur une ligne, on tasse de "un pixel" (32bits) */
-			++curout;
+			++curOut;
+			++currentInPixel;
 		}
+		curOut += rowGap;
+		currentInPixel += rowGap;
 	}
 
 }
